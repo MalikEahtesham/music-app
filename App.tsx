5a741115aed5579a9864c0b1a2a4e0998c9e43f1
MalikEahtesham <muhammadeahtesham@gmail.com>
2024-08-06 04:32:27 +0500

    done navigation

--- App.tsx
+++ App.tsx
@@ -7,179 +7,156 @@
 
 import React from 'react';
 import type { PropsWithChildren } from 'react';
-import { FlatList, Image, SafeAreaView, StatusBar, StyleSheet, Text, TextInput, View } from 'react-native';
+import { FlatList, Image, SafeAreaView, StatusBar, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { rMS, rS, rV } from './src/shared/responsive';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-
+import Home from './src/screens/Home';
+import Dummy from './src/screens/Dummy';
 type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
 
+const CustomTabBar = ({ state, descriptors, navigation }: any) => {
+  return (
+    <View
+      style={{
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+        alignItems: 'center',
+        backgroundColor: 'transparent',
+      }}
+    >
+      <View
+        style={{
+          flexDirection: 'row',
+          backgroundColor: '#0A091E',
+          width: '98%',
+          height: 60,
+          borderTopLeftRadius: 30,
+          borderTopRightRadius: 30,
+          shadowColor: '#A8BACF',
+          shadowOffset: { width: 0, height: -5 },
+          shadowOpacity: 0.4,
+          shadowRadius: 20,
+          elevation: 50, // Increased elevation for clearer shadow on Android
+        }}
+      >
+        {state.routes.map((route: any, index: any) => {
+          const { options } = descriptors[route.key];
+          const label =
+            options.tabBarLabel !== undefined
+              ? options.tabBarLabel
+              : options.title !== undefined
+                ? options.title
+                : route.name;
+
+          const isFocused = state.index === index;
+
+          const onPress = () => {
+            const event = navigation.emit({
+              type: 'tabPress',
+              target: route.key,
+              canPreventDefault: true,
+            });
+
+            if (!isFocused && !event.defaultPrevented) {
+              navigation.navigate(route.name);
+            }
+          };
+
+          const onLongPress = () => {
+            navigation.emit({
+              type: 'tabLongPress',
+              target: route.key,
+            });
+          };
+
+          return (
+            <TouchableOpacity
+              key={route.key}
+              accessibilityRole="button"
+              accessibilityState={isFocused ? { selected: true } : {}}
+              accessibilityLabel={options.tabBarAccessibilityLabel}
+              testID={options.tabBarTestID}
+              onPress={onPress}
+              onLongPress={onLongPress}
+              style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+            >
+              <Text style={{ color: isFocused ? 'tomato' : 'gray' }}>{label}</Text>
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    </View>
+  );
+};
 function App(): React.JSX.Element {
 
-  const AlbummList: any = [
-    {
-      "id": 1,
-      title: "The triangle",
-      image: require('./src/assets/Rectangle_16.png')
-    },
-    {
-      "id": 2,
-      title: "Dune Of Visa",
-      image: require('./src/assets/Rectangle_17.png')
-    },
-    {
-      "id": 3,
-      title: "Riskitall",
-      image: require('./src/assets/Rectangle_18.png')
-    },
-    {
-      "id": 4,
-      title: "Riskitall Of Visa",
-      image: require('./src/assets/Rectangle_16.png')
-    }
-  ]
-
-  const songList: any = [
-    {
-      "id": 1,
-      title: "Take care of you",
-      singer: 'Admina Thembi',
-      view: '114k / steams',
-      image: require('./src/assets/Rectangle_16.png')
-    },
-    {
-      "id": 2,
-      title: "The stranger inside you",
-      singer: 'Jeane Lebras',
-      view: '60.5k / steams',
-      image: require('./src/assets/Rectangle_17.png')
-    },
-    {
-      "id": 3,
-      title: "Edwall of beauty mind ",
-      singer: 'Jacob Givson',
-      view: '44.3k / steams',
-      image: require('./src/assets/Rectangle_18.png')
-    },
-    {
-      "id": 4,
-      title: "Riskitall Of Visa",
-      singer: 'Admina Thembi',
-      view: '114k / steams',
-      image: require('./src/assets/Rectangle_16.png')
-    },
-    {
-      "id": 3,
-      title: "Edwall of beauty mind ",
-      singer: 'Jacob Givson',
-      view: '44.3k / steams',
-      image: require('./src/assets/Rectangle_18.png')
-    },
-    {
-      "id": 3,
-      title: "Edwall of beauty mind ",
-      singer: 'Jacob Givson',
-      view: '44.3k / steams',
-      image: require('./src/assets/Rectangle_18.png')
-    },
-  ]
-  type ItemProps = { title: string, image: any };
-
-  const Item = ({ title, image }: ItemProps) => (
-    <View style={styles.item}>
-      <Image
-        source={image}
-        style={{ height: rV(90), borderRadius: rS(20), width: rS(120), alignSelf: 'center' }}
-      />
-      <Text style={styles.title}>{title}</Text>
-    </View>
-  );
-
-  type ItemListProps = { title: string, image: any, singer: string };
-
-  const ItemNew = ({ title, image, singer }: ItemListProps) => (
-    <View style={styles.itemNew}>
-      <Image
-        source={image}
-        style={{ height: rV(80), borderRadius: rS(20), width: rS(100), alignSelf: 'center' }}
-      />
-      <View style={{marginHorizontal:rS(5)}}>
-
-        <Text style={styles.titleNew}>{title}</Text>
-        <Text style={styles.views}>{singer}</Text>
-        <Text style={styles.views}>44.3k / steams</Text>
-
-      </View>
-
-    </View>
-  );
+  const Stack = createNativeStackNavigator();
+  const Tab = createBottomTabNavigator();
 
   return (
-    <SafeAreaView style={styles.sectionContainer}>
-      <View style={styles.header}>
-        <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingTop: rS(3),paddingBottom:rS(3), alignItems: 'center' }}>
-          <Image
-            source={require('./src/assets/dp.png')}
-            style={{ borderRadius: rS(20), height: rV(40), width: rS(40) }}
-            resizeMethod='resize'
-          />
-          <View style={{ marginLeft: rS(8) }}>
-            <Text style={{ color: '#fff', fontSize: rS(18), fontWeight: '600' }}>Sarwar Jahan</Text>
-            <Text style={{ color: '#fff', fontSize: rS(14) }}>Gold Member</Text>
-          </View>
-        </View>
-        <Image
-          source={require('./src/assets/Icon.png')}
-          style={{ height: rV(20), width: rS(20), alignSelf: 'center' }}
-        />
-      </View>
+    <NavigationContainer>
+      {/* <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator> */}
+      <Tab.Navigator tabBar={(props) => <CustomTabBar {...props} />}
 
-      <View style={{ flexDirection: 'row', marginVertical: rS(10) }}>
-        <View style={{ flex: .6 }}>
-          <Text style={{ color: '#fff', fontSize: rS(20), }}>Listen The</Text>
-          <Text style={{ color: '#fff', fontSize: rS(20) }}>Latest Musics</Text>
-        </View>
-        <View style={{ flex: .5,justifyContent:'center' }}>
-          <TextInput
-            placeholder='Search'
-            style={{borderRadius:rS(20)}}
-          />
-        </View>
+      // screenOptions={({ route }) => ({
+      //   tabBarStyle: {
+      //     backgroundColor: '#0A091E', // Background color of the tab bar
+      //     borderTopWidth: 0, // Remove top border
+      //     // elevation: 5, // Shadow for Android
+      //     height: 60, // Height of the tab bar
+      //     // shadowColor: '#A8BACF',
+      //     // shadowOffset: { width: 4, height: 3 },
+      //     // shadowOpacity: 5.27,
+      //     // shadowRadius: 4.65,
+      //     elevation: 6,
+      //     shadowColor: '#A8BACF',
+      //     shadowOffset: { width: 0, height: -5 },
+      //     shadowOpacity: 0.1,
+      //     shadowRadius: 20,
+      //     borderTopLeftRadius:30,
+      //     borderTopRightRadius:30,
+      //     width:'98%',
+      //     alignSelf:'center'
+      //   },
 
-      </View>
+      //   tabBarIcon: ({ focused, color, size }) => {
+      //     let iconName: any = '';
 
+      //     if (route.name === 'Home') {
+      //       iconName = focused
+      //         ? 'ios-information-circle'
+      //         : 'ios-information-circle-outline';
+      //     } else if (route.name === 'Settings') {
+      //       iconName = focused ? 'ios-list' : 'ios-list-outline';
+      //     }
 
-      <View style={{ marginVertical: rS(5) }}>
-        <Text style={{ color: '#fff', fontSize: rS(18), fontWeight: '400' }}>Recently Played</Text>
-        <FlatList
-
-          // style={{flex: 1}}
-          horizontal
-          data={AlbummList}
-          renderItem={({ item }) => <Item title={item.title} image={item.image} />}
-          keyExtractor={item => item.id}
-        // contentContainerStyle={{ flexGrow: 1, }}
-        />
-      </View>
-      <View style={{ flexGrow: 1 }}>
-        <Text style={{ color: '#fff', fontSize: rS(16),fontWeight:'400' }}>Recommend for you</Text>
-
-        <View style={{ flex: 1 }}>
-          <FlatList
-            // style={{flex: 1}}
-            // horizontal
-            data={songList}
-            renderItem={({ item }) => <ItemNew title={item.title} image={item.image} singer={item.singer} />}
-            keyExtractor={item => item.id}
-            // contentContainerStyle={{ flexGrow: 1, }}
-            ListFooterComponent={<View style={{ height: 20 }} />}
-          />
-        </View>
-      </View>
-      {/* </View> */}
-    </SafeAreaView>
+      //     // You can return any component that you like here!
+      //     return null//  <Ionicons name={iconName} size={size} color={color} />;
+      //   },
+      //   tabBarActiveTintColor: 'tomato',
+      //   tabBarInactiveTintColor: 'gray',
+      // })}
+      >
+        <Tab.Screen name="Home" component={Home} options={{
+          headerShown: false,
+          tabBarBadge: 3
+        }} />
+        <Tab.Screen name="Settings" component={Dummy} options={{
+          headerShown: false,
+          tabBarBadge: 3
+        }}/>
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -213,7 +190,7 @@ const styles = StyleSheet.create({
   },
   itemNew: {
     flexDirection: 'row',
-    alignItems:'center',
+    alignItems: 'center',
     // backgroundColor:'red',
     flex: 1,
     padding: rS(3),
---
+++ src/screens/Home.tsx
@@ -0,0 +1,249 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type { PropsWithChildren } from 'react';
+import { FlatList, Image, SafeAreaView, StatusBar, StyleSheet, Text, TextInput, View } from 'react-native';
+import { rV,rS,rMS} from '../shared/responsive';
+
+
+
+type SectionProps = PropsWithChildren<{
+  title: string;
+}>;
+
+
+function Home(): React.JSX.Element {
+
+  const AlbummList: any = [
+    {
+      "id": 1,
+      title: "The triangle",
+      image: require('../assets/Rectangle_16.png') //../assets/Rectangle_16.png
+    },
+    {
+      "id": 2,
+      title: "Dune Of Visa",
+      image: require('../assets/Rectangle_17.png')
+    },
+    {
+      "id": 3,
+      title: "Riskitall",
+      image: require('../assets/Rectangle_18.png')
+    },
+    {
+      "id": 4,
+      title: "Riskitall Of Visa",
+      image: require('../assets/Rectangle_16.png')
+    }
+  ]
+
+  const songList: any = [
+    {
+      "id": 1,
+      title: "Take care of you",
+      singer: 'Admina Thembi',
+      view: '114k / steams',
+      image: require('../assets/Rectangle_16.png')
+    },
+    {
+      "id": 2,
+      title: "The stranger inside you",
+      singer: 'Jeane Lebras',
+      view: '60.5k / steams',
+      image: require('../assets/Rectangle_17.png')
+    },
+    {
+      "id": 3,
+      title: "Edwall of beauty mind ",
+      singer: 'Jacob Givson',
+      view: '44.3k / steams',
+      image: require('../assets/Rectangle_18.png')
+    },
+    {
+      "id": 4,
+      title: "Riskitall Of Visa",
+      singer: 'Admina Thembi',
+      view: '114k / steams',
+      image: require('../assets/Rectangle_16.png')
+    },
+    {
+      "id": 3,
+      title: "Edwall of beauty mind ",
+      singer: 'Jacob Givson',
+      view: '44.3k / steams',
+      image: require('../assets/Rectangle_18.png')
+    },
+    {
+      "id": 3,
+      title: "Edwall of beauty mind ",
+      singer: 'Jacob Givson',
+      view: '44.3k / steams',
+      image: require('../assets/Rectangle_18.png')
+    },
+  ]
+  type ItemProps = { title: string, image: any };
+
+  const Item = ({ title, image }: ItemProps) => (
+    <View style={styles.item}>
+      <Image
+        source={image}
+        style={{ height: rV(90), borderRadius: rS(20), width: rS(120), alignSelf: 'center' }}
+      />
+      <Text style={styles.title}>{title}</Text>
+    </View>
+  );
+
+  type ItemListProps = { title: string, image: any, singer: string };
+
+  const ItemNew = ({ title, image, singer }: ItemListProps) => (
+    <View style={styles.itemNew}>
+      <Image
+        source={image}
+        style={{ height: rV(80), borderRadius: rS(20), width: rS(100), alignSelf: 'center' }}
+      />
+      <View style={{marginHorizontal:rS(5)}}>
+
+        <Text style={styles.titleNew}>{title}</Text>
+        <Text style={styles.views}>{singer}</Text>
+        <Text style={styles.views}>44.3k / steams</Text>
+
+      </View>
+
+    </View>
+  );
+
+  return (
+    <SafeAreaView style={styles.sectionContainer}>
+      <View style={styles.header}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingTop: rS(3),paddingBottom:rS(3), alignItems: 'center' }}>
+          <Image
+            source={require('../assets/dp.png')}
+            style={{ borderRadius: rS(20), height: rV(40), width: rS(40) }}
+            resizeMethod='resize'
+          />
+          <View style={{ marginLeft: rS(8) }}>
+            <Text style={{ color: '#fff', fontSize: rS(18), fontWeight: '600' }}>Sarwar Jahan</Text>
+            <Text style={{ color: '#fff', fontSize: rS(14) }}>Gold Member</Text>
+          </View>
+        </View>
+        <Image
+          source={require('../assets/Icon.png')}
+          style={{ height: rV(20), width: rS(20), alignSelf: 'center' }}
+        />
+      </View>
+
+      <View style={{ flexDirection: 'row', marginVertical: rS(10) }}>
+        <View style={{ flex: .6 }}>
+          <Text style={{ color: '#fff', fontSize: rS(20), }}>Listen The</Text>
+          <Text style={{ color: '#fff', fontSize: rS(20) }}>Latest Musics</Text>
+        </View>
+        <View style={{ flex: .5,justifyContent:'center' }}>
+          <TextInput
+            placeholder='Search'
+            style={{borderRadius:rS(20)}}
+          />
+        </View>
+
+      </View>
+
+
+      <View style={{ marginVertical: rS(5) }}>
+        <Text style={{ color: '#fff', fontSize: rS(18), fontWeight: '400' }}>Recently Played</Text>
+        <FlatList
+
+          // style={{flex: 1}}
+          horizontal
+          data={AlbummList}
+          renderItem={({ item }) => <Item title={item.title} image={item.image} />}
+          keyExtractor={item => item.id}
+        // contentContainerStyle={{ flexGrow: 1, }}
+        />
+      </View>
+      <View style={{ flexGrow: 1 }}>
+        <Text style={{ color: '#fff', fontSize: rS(16),fontWeight:'400' }}>Recommend for you</Text>
+
+        <View style={{ flex: 1 }}>
+          <FlatList
+            // style={{flex: 1}}
+            // horizontal
+            data={songList}
+            renderItem={({ item }) => <ItemNew title={item.title} image={item.image} singer={item.singer} />}
+            keyExtractor={item => item.id}
+            // contentContainerStyle={{ flexGrow: 1, }}
+            ListFooterComponent={<View style={{ height: 20 }} />}
+          />
+        </View>
+      </View>
+      {/* </View> */}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    flex: 1,
+    // marginLeft: 10,
+    // marginTop: StatusBar.currentHeight || 0,
+    paddingVertical: rV(5),
+    paddingHorizontal: rV(10),
+    // marginRight: 10,
+    backgroundColor: '#0A071E'
+  },
+  header: {
+    // width: '100%',
+    // backgroundColor: 'red',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  item: {
+    height: rV(120),
+    // backgroundColor: '#f9c2ff',
+    padding: rS(5),
+    marginVertical: rS(3),
+    // marginHorizontal: rS(3),
+  },
+  itemNew: {
+    flexDirection: 'row',
+    alignItems:'center',
+    // backgroundColor:'red',
+    flex: 1,
+    padding: rS(3),
+    marginVertical: rS(5),
+    marginHorizontal: rS(2),
+  },
+  title: {
+    fontSize: rS(14),
+    color: '#fff',
+    alignSelf: 'center'
+  },
+  titleNew: {
+    fontSize: rS(14),
+    color: '#fff',
+    // alignSelf: 'center'
+  },
+  views: {
+    fontSize: rS(12),
+    color: '#DEDEDE',
+    // alignSelf: 'center'
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default Home;
